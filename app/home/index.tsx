980242e6692c138ff3ba76e3c1367e2324428a87
Afras-cyber <afras975@gmail.com>
2025-02-26 22:40:16 +0530

    🎨  : clean the code

---
+++ app/home/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text className="text-primary">Home Section</Text>
+    </View>
+  );
+};
+
+export default Home;
---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text className="text-primary">Home Primary</Text>
+    </View>
+  );
+};
+
+export default Home;
